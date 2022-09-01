066707bcd5019f0eddee0e40ea501e5feba2173b
wonghye <dearhw117@naver.com>
2022-09-01 18:15:37 +0900

    0901

--- springdb/tbl_member_auth.sql
+++ springdb/tbl_member_auth.sql
@@ -21,10 +21,14 @@ SELECT mem.userid, userpw, username, enabled, regdate, updatedate, auth
 FROM tbl_member mem 
     LEFT OUTER JOIN tbl_member_auth auth
     ON mem.userid = auth.userid
-WHERE mem.userid = 'admin90';    
+WHERE mem.userid = 'admin10';    
 
 
-SELECT * FROM tbl_member;
+SELECT * FROM tbl_member ORDER BY regdate DESC;
 SELECT * FROM tbl_member_auth;
 
+DELETE tbl_member WHERE userid = 'cloud';
+DELETE tbl_member_auth WHERE userid = 'cloud';
+
 DROP TABLE tbl_member;
+DROP TABLE tbl_member_auth;

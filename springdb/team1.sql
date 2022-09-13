188418420199663f7b07e7278deeb7082ea3acd4
wonghye <dearhw117@naver.com>
2022-09-13 18:13:36 +0900

    0913

--- springdb/team1.sql
+++ springdb/team1.sql
@@ -20,6 +20,44 @@ CREATE TABLE tbl_horror(
     likeNum NUMBER(5) DEFAULT 0
 );
 
+-- tbl_member 테이블 생성
+CREATE TABLE tbl_member(
+    userid      VARCHAR2(50) PRIMARY KEY,
+    userpw       VARCHAR2(100) NOT NULL,
+    username   VARCHAR2(100) NOT NULL,
+    regdate       DATE DEFAULT SYSDATE,
+    updatedate  DATE DEFAULT SYSDATE,
+    enabled       CHAR(1) DEFAULT '1',
+    email       VARCHAR2(50) NOT NULL
+);
+
+--권한 인증 테이블
+CREATE TABLE tbl_member_auth(
+    auth    VARCHAR2(50) NOT NULL,
+    userid  VARCHAR2(50) NOT NULL,
+    CONSTRAINT fk_member_auth FOREIGN KEY (userid)
+    REFERENCES tbl_member(userid) ON DELETE CASCADE
+);
+
+--관리자 권한 변경
+UPDATE tbl_member_auth SET auth='ROLE_ADMIN' WHERE userid='admin';
+
+-- 외부 조인 : 특정 member의 회원정보와 권한 정보를 출력
+SELECT mem.userid, userpw, username, enabled, regdate, updatedate, auth FROM tbl_member mem
+    LEFT OUTER JOIN tbl_member_auth auth
+    ON mem.userid = auth.userid
+WHERE mem.userid = 'admin90';
+
+
+SELECT * FROM tbl_member;
+SELECT * FROM tbl_member_auth;
+
+DROP TABLE tbl_member;
+DROP TABLE tbl_member_auth;
+
+COMMIT;
+
+
 --자동 번호
 CREATE SEQUENCE seq;
 CREATE SEQUENCE humorSeq; --유머 자동 번호
